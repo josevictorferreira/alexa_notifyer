eddf86891ca46fa3dec556caf7c97d08b94ef2d6
José Victor Ferreira <josevictorferreira@outlook.com>
2020-11-21 04:17:19 -0300

    added spec for create event service

--- spec/services/events/create_service_spec.rb
+++ spec/services/events/create_service_spec.rb
@@ -8,4 +8,13 @@ describe Events::CreateService do
   let(:params) { { description: description, date: date } }
   let(:description) { FFaker::Lorem.paragraph(1) }
   let(:date) { FFaker::Time.date }
+
+  context 'when success' do
+    let(:service_perform) { service.perform }
+
+    it { expect(service_perform).to be_a_success }
+    it { expect(service_perform.event.description).to eq(description) }
+    it { expect(service_perform.event.date).to eq(date) }
+    it { expect { service_perform }.to change(Event, :count).by(1) }
+  end
 end
